1459fc87be72b6f1555dc3d500b09e01472c64f1
Juan Villa <ing.villa.jimenez@gmail.com>
2024-11-28 18:52:39 -0600

    init

---
+++ src/Calculator.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+class Calculator
+{
+    public function add($a, $b)
+    {
+        $password = "testroot";
+    
+        echo $password;
+    
+        return $a + $b;
+    }
+}
